7fc900987b63f2d94442518e2dbae5f36b78ac55
theobdn <theobdnwork@gmail.com>
2022-04-22 01:07:11 +0200

    Création navbar

--- m2iapp/src/App.tsx
+++ m2iapp/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
+import { Navbar } from './Primary/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar/>
+      <h1>App page</h1>
       <Button variant='contained'>TEST</Button>
     </div>
   );
